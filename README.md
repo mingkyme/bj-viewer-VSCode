a15f6eebb05ff56bd34f24d19aac9584e92cd291
MINGKYME <admin@mingky.me>
2022-02-13 16:54:33 +0900

    자동 실행 코드 구현

--- README.md
+++ README.md
@@ -4,8 +4,6 @@
 
 숫자로 이뤄진 코드 파일을 Open 합니다.
 
-Command Palette 에서 BJ Open을 이용하여 명령을 실행 합니다.
-
 다음과 같은 화면을 볼 수 있습니다.
 
 ![image](https://raw.githubusercontent.com/mingkyme/bj-viewer-VSCode/main/images/screenshot01.png)
--- src/extension.ts
+++ src/extension.ts
@@ -5,32 +5,37 @@ import * as request from 'request';
 // this method is called when your extension is deactivated
 export function deactivate() { }
 
+let panel : vscode.WebviewPanel | undefined;
 export async function activate(context: vscode.ExtensionContext) {
+	vscode.window.onDidChangeActiveTextEditor((e) => {
+		if (e && e.document.fileName) {
+			let fileName = e.document.fileName.split('/').pop();
+				fileName = fileName?.substring(0, fileName.lastIndexOf('.'));
+				if (fileName) {
+					let bjNumber = +fileName;
+					if (!isNaN(bjNumber)) {
+						vscode.window.showInformationMessage(fileName);
+						if(!panel){
+							vscode.window.showInformationMessage("Here");
+							panel = vscode.window.createWebviewPanel('BJ','BJ',vscode.ViewColumn.Two);
+						}
+						panel.onDidDispose((e)=>{
+							panel = undefined;
+						});
 
-	let disposable = vscode.commands.registerCommand('bj-viewer.open', () => {
-		if (vscode.window.activeTextEditor) {
-			var fileName = path.basename(vscode.window.activeTextEditor.document.fileName).split('.')[0];
-			let bjNumber = +fileName;
-			if (!isNaN(bjNumber)) {
-				vscode.window.showInformationMessage(bjNumber.toString());
-				const panel = vscode.window.createWebviewPanel(
-					'BJ',
-					`BJ ${bjNumber}`,
-					vscode.ViewColumn.Two,
-					{}
-				);
-				request.get("https://www.acmicpc.net/problem/" + bjNumber, (err, res) => {
-					if (err) {
-						vscode.window.showErrorMessage("Unknown Error");
-						console.log(err);
-						panel.dispose();
+						panel.title = `BJ ${bjNumber}`;
+						request.get("https://www.acmicpc.net/problem/" + bjNumber, (err, res) => {
+							if (err) {
+								vscode.window.showErrorMessage("Unknown Error");
+								panel?.dispose();
+							}
+							if(panel){
+								panel.webview.html = res.body;
+							}
+							
+						});
 					}
-					panel.webview.html = res.body;
-				});
-			}
+				}
 		}
 	});
-
-	context.subscriptions.push(disposable);
-
 }
